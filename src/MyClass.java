8739e9e1866e3dc7f5ecc1a1f592220a4259f3d8
lee <leeparker@outlook.com>
2023-07-17 14:26:52 +0100

    first upload

---
+++ src/MyClass.java
@@ -0,0 +1,23 @@
+public class MyClass {
+    public static void main(String[] args){
+        System.out.println("This is a Java course");
+        System.out.println(methodOne());
+        System.out.println((methodName("Lee")));
+        Main runner = new Main();
+        String myVar = "shrek";
+        System.out.println((runner.myMethod(myVar)));
+    }
+
+    public static String methodOne(){
+        return "This is th first day of the Java course!";
+    }
+
+    public static String methodName(String userInput){
+        return "Hello, my name is " + userInput;
+    }
+
+    public String myMethod( String input){
+        return "My favourite film is" +input;
+    }
+
+}
